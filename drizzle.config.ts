21c83d0be2e0e5a67575432daa93c612198d9d43
muhamadaguss <muhamadagus3197@gmail.com>
2026-03-31 00:54:41 +0700

    refactor: update drizzle configuration to use index-based schema path and remove verbose settings

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,33 +1,14 @@
-// import { defineConfig } from "drizzle-kit";
-// import path from "path";
-
-// if (!process.env.DATABASE_URL) {
-//   throw new Error("DATABASE_URL, ensure the database is provisioned");
-// }
-
-// export default defineConfig({
-//   schema: path.join(__dirname, "./src/db/schema/index.ts"),
-//   dialect: "postgresql",
-//   dbCredentials: {
-//     url: process.env.DATABASE_URL,
-//   },
-// });
 import { defineConfig } from "drizzle-kit";
-// Tambahkan import dotenv agar DATABASE_URL pasti terbaca saat push dari terminal
-import "dotenv/config";
+import path from "path";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is missing in .env file");
+  throw new Error("DATABASE_URL, ensure the database is provisioned");
 }
 
 export default defineConfig({
-  // Ubah path ini agar langsung membaca semua file schema yang ada di folder tersebut
-  schema: "./src/db/schema/**/*.ts", // atau "./src/db/schema.ts" jika hanya 1 file
+  schema: path.join(__dirname, "./src/db/schema/index.ts"),
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
-  // Tambahkan ini agar output terminal lebih detail saat ada error
-  verbose: true,
-  strict: true,
 });
